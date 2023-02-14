ce06688f8328a8deb35faae7f5b5120cfa4b5d0e
unknown <nacho.morales5mail.com>
2023-02-14 19:54:26 -0300

    Henry dia 22

--- 12-React-Redux/homework/01 - Exercises/src/components/Products/Products.jsx
+++ 12-React-Redux/homework/01 - Exercises/src/components/Products/Products.jsx
@@ -1,24 +1,43 @@
 import { connect } from 'react-redux';
 import React from 'react';
 import './products.css';
-// import Card from '../Card/Card'
+import Card from '../Card/Card';
+import { getStoreName } from '../../redux/actions/actions';
 
-export function Products() {
-   return (
-      <>
-         <div className='productsBg'>
-            <h1 className='productsTl'></h1>
+export function Products({ list, storeName, getStoreName }) {
+	React.useEffect(() => {
+		getStoreName();
+	}, []);
 
-            <div className='productsList'>
-               {/* ¡Renderiza aquí todas tus cards! */}
-            </div>
-         </div>
-      </>
-   );
+	return (
+		<>
+			<div className="productsBg">
+				<h1 className="productsTl">{storeName}</h1>
+
+				<div className="productsList">
+					{/* ¡Renderiza aquí todas tus cards! */}
+					{list.map((product) => {
+						return (
+							<Card
+								name={product.name}
+								price={product.price}
+								id={product.id}
+								key={product.id}
+							/>
+						);
+					})}
+				</div>
+			</div>
+		</>
+	);
 }
 
-export function mapStateToProps() {}
+export function mapStateToProps(state) {
+	return { list: state.list, storeName: state.storeName };
+}
 
-export function mapDispatchToProps() {}
+export function mapDispatchToProps(dispatch) {
+	return { getStoreName: () => dispatch(getStoreName()) };
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Products);
